7f5d5e04ccb1ae2917da422ed458e1b7bf739b28
Zahra Hussain🤍 <zzvakil6@gmail.com>
2024-03-05 22:36:31 +0530

    done

---
+++ main.js
@@ -0,0 +1,147 @@
+const iconToggle = document.querySelector('.toggle_icon');
+const navbarMenu= document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu_link');
+const iconClose = document.querySelector('.close_icon');
+
+iconToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
+});
+
+iconClose.addEventListener('click', () => {
+    navbarMenu.classList.remove('active');
+});
+
+menuLinks.forEach((menuLink) => {
+
+    menuLink.addEventListener('click', () => {
+         navbarMenu.classList.remove('active');
+
+})
+});
+
+//dark mode
+
+const toggleSwitch = document.getElementById('toggle_switch');
+const moonIcon = document.getElementById('moon');
+const sunIcon = document.getElementById('sun');
+const body = document.body;
+
+toggleSwitch.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    const isDarkMode = body.classList.contains('dark');
+    if (isDarkMode) {
+        moonIcon.style.opacity = '0';
+        sunIcon.style.opacity = '1';
+    } else {
+        moonIcon.style.opacity = '1';
+        sunIcon.style.opacity = '0';
+    }
+});
+
+
+// chnge bkg heaeder 
+
+function scrollHeader(){
+    const header = document.querySelectorId('header');
+    this.scrollY >=20 ? header.classList.add('active') : header.classList.remove('active');
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+//typed change 
+const typed = document.querySelector('.typed');
+if(typed){
+    let typed_strings = typed.getAttribute('data-typed-items');
+    typed_strings= typed_strings.split(',');
+    new Typed('.typed',{
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+    });
+}
+
+//active link scroll
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(section =>{
+        const sectionHeight = section.offsetHeight;
+        const sectionTop = section.offsetTop - 50;
+
+        let sectionId = section.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY<= sectionTop + sectionHeight){
+            document.querySelector('.menu a[href *=' + sectionId + ']').classList.add('active-link');
+         } else{
+            document.querySelector('.menu a[href *=' + sectionId + ']').classList.remove('active-link');
+
+         }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
+
+// Resume scroll
+
+const pages = document.querySelectorAll('.page');
+const resume = document.querySelector('.resume');
+
+function resumeActive() {
+    const scrolly = window.pageYOffset;
+    pages.forEach(page => {
+        const sectionHeight = page.offsetHeight;    
+        const sectionTop = page.offsetTop - 150;
+       
+        let sectionId = page.getAttribute('id');
+        
+            if (scrollY > sectionTop && scrolly <= sectionTop + sectionHeight){
+            document.querySelector('.resume_tabs a[href *=' + sectionId + ']').classList.add('current');
+            }else{
+                document.querySelector('.resume_tabs a[href *=' + sectionId + ']').classList.remove('current');
+
+            }
+        })
+}
+window.addEventListener('scroll', resumeActive);
+
+
+
+// Select the "Download Resume" link
+const downloadResumeLink = document.querySelector('.btn-download');
+
+// Add a click event listener to the link
+downloadResumeLink.addEventListener('click', (event) => {
+    // Prevent the default action of the link (i.e., opening a new page)
+    event.preventDefault();
+    
+    // Create a temporary anchor element
+    const anchor = document.createElement('a');
+    // Set the href attribute to the URL of your resume PDF file
+    anchor.href = 'Resume.pdf';
+    // Set the download attribute to specify the filename for the downloaded file
+    anchor.download = 'Resume.pdf';
+    // Programmatically trigger a click event on the anchor element to start the download
+    anchor.click();
+});
+
+//contact form
+
+const form = document.querySelector('form');
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
